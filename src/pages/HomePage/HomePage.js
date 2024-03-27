f0b9853cd877077e80a1af26d5b523087c9093d6
Chaia Billan <chaia_billan@yahoo.com>
2024-03-26 22:44:04 -0700

    made layout and styled homepage according to mockups

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,49 @@
 import './HomePage.scss'
 import { Link } from 'react-router-dom'
+import lagos from '../../assets/images/IMG_3999.jpeg'
 
 function HomePage () {
 
     return (
         <>
-            <p>this is the home page</p>
-            <Link to="/resources?selectedResource=emergency-number">Go to Resources Page with Emergency Number</Link>
-
-
+            <section className='hero' >
+                <img className='hero__image' src={lagos}/>
+                <p className='hero__title'>
+                    Discover the world without limits. 
+                </p>
+            </section>
+            <section>
+                introduction
+            </section>
+            <section className='spotlight-section'>
+                <p className='spotlight-section__title'>In the spotlight...</p>
+                <ul className='spotlight'>
+                    <Link to="/resources?selectedResource=emergency-number" className='spotlight__item'>
+                        <li>Ambulance Numbers<br></br> from <br></br> Around The World</li>
+                    </Link>
+                    <Link to='/about' className='spotlight__item'>
+                        <li>Meet the author!</li>
+                    </Link>
+                    <Link to='/community' className='spotlight__item'>
+                        <li>Connect With<br></br> Fellow Travelers</li>
+                    </Link>
+                    <Link to ='/travel-guide' className='spotlight__item'>
+                        <li>Read the <br></br> comprehensive <br></br> Travel Guide</li>
+                    </Link>
+                    <Link to='#' className='spotlight__item'>
+                        <li></li>
+                    </Link>
+                    <Link to='#' className='spotlight__item'>
+                        <li></li>
+                    </Link>
+                    <Link to='#' className='spotlight__item'>
+                        <li></li>
+                    </Link>
+                    <Link to='#' className='spotlight__item'>
+                        <li></li>
+                    </Link>
+                </ul>
+            </section>
         </>
     )
 }
