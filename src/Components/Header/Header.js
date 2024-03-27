0c55f565d84e1cbeac2907a1c5a7f83e320c6140
Chaia Billan <chaia_billan@yahoo.com>
2024-03-26 21:54:56 -0700

    create nav bar styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,26 @@
 import './Header.scss'
+import { Link } from 'react-router-dom'
 
 function Header () {
 
     return (
-        <p>this is the header</p>
+        <section className='nav-bar'>
+            <Link to='/travel-guide' className='nav-bar__option'>
+                Travel Guide
+            </Link>
+            <Link to='/community' className='nav-bar__option'>
+                Community
+            </Link>
+            <Link to='/' className='nav-bar__option nav-bar__option--title '>
+                SafeTravels Allergy Compass
+            </Link>
+            <Link to='/resources' className='nav-bar__option'>
+                Resources
+            </Link>
+            <Link to='/about' className='nav-bar__option'>
+                About
+            </Link>
+        </section>
     )
 }
 
